69f6cd15b0164a641d297e35418b6bef683e6a42
Johannes Nilsson <johannes@nilsson.im>
2021-01-06 17:52:05 +0100

    Change api url

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-   baseURL: 'http://localhost:5000/api',
+   baseURL: 'https://fast-wildwood-30146.herokuapp.com/api',
    headers: {
          "Content-type": "application/json"
    }
